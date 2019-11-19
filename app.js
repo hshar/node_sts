c584d8b28ecaed3de077854eea4b12bb7a5526d7
hshar <hemantsharma@Hemants-MacBook-Air.local>
2019-11-20 03:17:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Licensed under the Apache-2.0 License on an "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND.   
+
+// ABOUT THIS NODE.JS SAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
+// https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sts-examples-policies.html
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'REGION'});
+
+var roleToAssume = {RoleArn: 'arn:aws:iam::697042039271:role/sts-admin',
+                    RoleSessionName: 'sts-admin',
+                    DurationSeconds: 900,};
+var roleCreds;
+
+// Create the STS service object    
+var sts = new AWS.STS({apiVersion: '2011-06-15'});
+
+//Assume Role
+sts.assumeRole(roleToAssume, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else{
+        roleCreds = {accessKeyId: data.Credentials.AccessKeyId,
+                     secretAccessKey: data.Credentials.SecretAccessKey,
+                     sessionToken: data.Credentials.SessionToken};
+        stsGetCallerIdentity(roleCreds);
+    }
+});
+
+//Get Arn of current identity
+function stsGetCallerIdentity(creds) {
+    var stsParams = {credentials: creds };
+    // Create STS service object
+    var sts = new AWS.STS(stsParams);
+        
+    sts.getCallerIdentity({}, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        }
+        else {
+            console.log(data.Arn);
+        }
+    });    
+}
